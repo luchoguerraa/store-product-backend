bce557c5ac925360c3afaa1a1beebc10cd0e6a1c
laluisguerra <lguerra@ext.bancointernacional.cl>
2021-01-06 00:15:00 -0300

    [@lguerra] agrega servicio de propductos y ofertas mas test unitarios y tests de ingración.

---
+++ products1/src/main/java/com/store/products1/Products1Application.java
@@ -0,0 +1,13 @@
+package com.store.products1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Products1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Products1Application.class, args);
+	}
+
+}
